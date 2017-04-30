10eb35e0a3a11509bd357153fb7c869b93974a31
Debmalya Sinha <debmalya.01@gmail.com>
2017-04-30 23:27:16 +0100

    scenario input almost done.
    need to fix a few connections in the init and in the generateIndices.

--- inputScene.m
+++ inputScene.m
@@ -11,44 +11,49 @@ global p_ij; %preference of nearby loc j given loc i. ii is pref 1. ij >1
 global r_ij; %response time of j to i. ii = 0.
 
 N = 8;
+
 epsilon_i = [8,8,8,8,8,8,8,8];
+
 p_ij = zeros(8,8);
 %            A B C D E F G H
 p_ij(1,:) = [1,2,3,4,6,8,5,7];
 p_ij(2,:) = [4,1,3,2,5,8,6,7];
-p_ij(3,:) = [4,3,6,2,7,8,4,5];
-p_ij(4,:) = [4,1,3,2,5,8,6,7];
-p_ij(5,:) = [4,3,6,2,7,8,4,5];
-p_ij(6,:) = [4,1,3,2,5,8,6,7];
-p_ij(7,:) = [4,3,6,2,7,8,4,5];
-p_ij(8,:) = [4,3,6,2,7,8,4,5];
+p_ij(3,:) = [6,3,1,2,7,8,4,5];
+p_ij(4,:) = [7,3,2,1,4,8,5,6];
+p_ij(5,:) = [8,6,4,3,1,2,5,7];
+p_ij(6,:) = [8,7,4,6,3,1,2,5];
+p_ij(7,:) = [8,7,4,5,6,2,1,3];
+p_ij(8,:) = [8,6,3,5,7,4,2,1];
 
 
 %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%% type of equipments
 %%%%%%%%%%%%%%%%%%%%%
 global R; %set of equipment types
-global betaR; %number of supporting generalist
 global alphaR; %number of specialists
+global betaR; %number of supporting generalist
 global C_r; % current capacity of type r equipment
 
-R = 5;
-
-betaR = zeros(1,R); 
-betaR(:) = 5; %change this with actual data
+R = 3; %IRU, USAR, HVP
 
 alphaR = zeros(1,R); 
-alphaR(:) = 5; %change this with actual data
+alphaR(:) = [10, 10, 2]; % actual data from table 3
+
+betaR = zeros(1,R); 
+betaR(:) = [20, 25, 5]; %actual data from table 3
 
 C_r = zeros(1,R);
-
+C_r = [72, 20, 46]; %from table 3
 
 %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% setup Time
 %%%%%%%%%%%%%%%%%%%%%
 global sr ;  %setup time for all r.
 global tij;
+
 sr = zeros(1,R); 
+sr = [60, 30, 60]; %setup times from table 3
+
 tij = 0;
 
 
@@ -59,16 +64,23 @@ global K; %disaster types
 global R_k; %equipments needed in case of k.
 global T_k; %time points for k where next resource is necessary
 
+K = 2; % terrorist attack and major flood. page 12.
+R_K(1,:) = [1,2]; %l = 1, bomb, need IRU, USAR, 1,2
+R_K(2,:) = [2,3]; %l = 2, flood, need USAR, HVP. 2,3
 
 %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% Scenarios
 %%%%%%%%%%%%%%%%%%%%%
 global S; %set of scenarios
 
+%for now, lets consider only 1 scenario with bomb in A, B, C.
+S = 1;
+
+
 %this here maps the l to a corresponding s value
 global whatL; %simultaneous disasters l's are happening in each s from S.
-whatL = zeros(1,maxV_(5));
-whatL(:) = maxV_(5);
+whatL = zeros(1,S);
+whatL(:) = 1;
 %here is a potential bug. if you don't update the j_ and only whatJ, the
 %indexes the resolver finds will be wrong.
 
